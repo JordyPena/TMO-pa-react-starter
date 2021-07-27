398a853b946c87135665558c74bd18226c19118f
Jordy Pena <jordypenamontes@gmail.com>
2021-07-27 10:31:38 -0500

    app.js -- refactored code -- on submit hide form, show button, clear inputs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 
 function App() {
-
-  const [ hideAddRecipe, setHideAddRecipe ] = useState(false);
-  const [ showForm, setShowForm ] = useState(false);
-  const [ recipeName, setRecipeName ] = useState("");
-  const [ recipeInstructions, setRecipeInstructions ] = useState("");
-  const [ recipes, setRecipes ] = useState([]);
+  const [hideAddRecipe, setHideAddRecipe] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [recipeName, setRecipeName] = useState("");
+  const [recipeInstructions, setRecipeInstructions] = useState("");
+  const [recipes, setRecipes] = useState([]);
 
   // hide button when clicked
   const hideButton = () => {
     setHideAddRecipe(true);
     setShowForm(true);
-  }
+  };
 
   // get recipe name/instructions from userinput
   const userInput = (e) => {
     const { name, value } = e.target;
     if (name === "name") {
-
       setRecipeName(value);
     } else if (name === "instructions") {
       setRecipeInstructions(value);
     }
-  }
+  };
 
   // handle form submit
   // save recipe
@@ -32,10 +30,15 @@ function App() {
     e.preventDefault();
     // add recipeName and instructions to an object
     //then add to recipes
-    const newRecipe = {name: recipeName, instructions: recipeInstructions}
+    const newRecipe = { name: recipeName, instructions: recipeInstructions };
     // add newRecipe to list and keep items already added
-    setRecipes((prevRecipes) => [...recipes, newRecipe])
-  }
+    setRecipes((prevRecipes) => [...recipes, newRecipe]);
+    //hide form show add recipe button
+    setHideAddRecipe(false);
+    setShowForm(false);
+    setRecipeName("");
+    setRecipeInstructions("");
+  };
 
   return (
     <div>
@@ -43,40 +46,46 @@ function App() {
       {/* ^ Do not remove this element ^ */}
       <h1>My Recipes</h1>
       {/* remove button when clicked */}
-      {hideAddRecipe === false ?  <button onClick={() => hideButton()}>Add recipe</button> : ""}
-     
-     {/* render my recipes and hide no recipes text when recipes exist */}
-      {recipes.length >=1 ? "" : <p>There are no recipes to list</p>}
-      {recipes.length >= 1 && recipes.map((recipe, idx) => {
-        return (
-          <div key={idx}>
-            <p>{recipe.name}</p>
-          </div>
-        )
-      })}
-      {/* render form when button is clicked */}
-      {showForm === true ?  <form onSubmit={(e) => formSubmitted(e)}>
-        <label>Recipe Name</label>
-        <input
-          type="text"
-          value={recipeName}
-          onChange={(e) => userInput(e)}
-          name="name"
-          required
-        />
-        <label>Recipe Instructions</label>
-        <input
-          type="text"
-          value={recipeInstructions}
-          onChange={(e) => userInput(e)}
-          name="instructions"
-          required
-        />
-        <button>Submit</button>
-      </form> : ""}
+      {hideAddRecipe === false ? (
+        <button onClick={() => hideButton()}>Add recipe</button>
+      ) : (
+        ""
+      )}
 
-     
-     
+      {/* render my recipes and hide no recipes text when recipes exist */}
+      {recipes.length >= 1 ? "" : <p>There are no recipes to list</p>}
+      {recipes.length >= 1 &&
+        recipes.map((recipe, idx) => {
+          return (
+            <div key={idx}>
+              <p>{recipe.name}</p>
+            </div>
+          );
+        })}
+      {/* render form when button is clicked */}
+      {showForm === true ? (
+        <form onSubmit={(e) => formSubmitted(e)}>
+          <label>Recipe Name</label>
+          <input
+            type="text"
+            value={recipeName}
+            onChange={(e) => userInput(e)}
+            name="name"
+            required
+          />
+          <label>Recipe Instructions</label>
+          <input
+            type="text"
+            value={recipeInstructions}
+            onChange={(e) => userInput(e)}
+            name="instructions"
+            required
+          />
+          <button>Submit</button>
+        </form>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
